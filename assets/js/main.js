764e8d8bef062715cd765ed25b379ce590387324
Ivan Andrus <iandrus@adobe.com>
2024-03-19 20:18:53 -0600

    Revert "fix: remove Footnote tooltip feature #379"
    This reverts commit e61518997850d171b1a9c322ce59a30f4633e1e0.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 import initMobileNavbar from './initMobileNavbar';
 import initToc from './initToc';
 import initHeaderAnchor from './initHeaderAnchor';
+import initFootnoteTooltip from './initFootnoteTooltip';
 
 /* main */
 $(document).ready(function () {
   initMobileNavbar();
   initToc();
   initHeaderAnchor();
+  initFootnoteTooltip();
 });
